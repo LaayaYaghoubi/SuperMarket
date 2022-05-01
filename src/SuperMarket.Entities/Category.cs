aab52e2d7aaf6b367b4d1a2f2dac96e1c52f1ce2
Laaya <layayaghoubi@gmail.com>
2022-05-01 10:04:25 +0430

    super market migration layer added to the project and tables created

--- src/SuperMarket.Entities/Category.cs
+++ src/SuperMarket.Entities/Category.cs
@@ -8,8 +8,16 @@ namespace SuperMarket.Entities
 {
     public class Category
     {
+        public Category()
+        {
+            products = new List<Product>() { };
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Product> products { get; set; }
+
+         
 
     }
 }
--- src/SuperMarket.Migrations/_202201050858_InitialDataBase.cs
+++ src/SuperMarket.Migrations/_202201050858_InitialDataBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentMigrator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,72 @@ using System.Threading.Tasks;
 
 namespace SuperMarket.Migrations
 {
-    internal class _202201050858_InitialDataBase
+    [Migration(202201050858)]
+    public class _202201050858_InitialDataBase : Migration
     {
+        public override void Up()
+        {
+            CreateProductsTable();
+
+            CreateCategoriesTable();
+
+            CreatePurchaseVouchersTable();
+
+            CreateSalesInvoicesTable();
+
+        }
+
+        private void CreateSalesInvoicesTable()
+        {
+            Create.Table("SalesInvoices")
+                .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("ClientName").AsString(50).NotNullable()
+                .WithColumn("Date").AsDateTime().NotNullable()
+                .WithColumn("TotalPrice").AsDecimal().NotNullable()
+                .WithColumn("NumberOfProducts").AsInt32().NotNullable()
+                .WithColumn("ProductId").AsInt32().NotNullable()
+                .ForeignKey("FK_SalesInvoice_Products", "Products", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreatePurchaseVouchersTable()
+        {
+            Create.Table("PurchaseVouchers")
+                .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("TotalPrice").AsDecimal().NotNullable()
+                .WithColumn("ProductId").AsInt32().NotNullable()
+                .ForeignKey("FK_PurchaseVoucher_Products", "Products", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreateCategoriesTable()
+        {
+            Create.Table("Categories")
+                .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("Name").AsString(50).NotNullable();
+        }
+
+        private void CreateProductsTable()
+        {
+            Create.Table("Products")
+                .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("Price").AsDecimal().NotNullable()
+                .WithColumn("Stock").AsInt32().Nullable()
+                .WithColumn("MinimumStock").AsInt32().NotNullable()
+                .WithColumn("MaximumStock").AsInt32().NotNullable()
+                .WithColumn("ExpirationDate").AsDateTime().Nullable()
+                .WithColumn("CategoryId").AsInt32().NotNullable()
+                .ForeignKey("FK_Products_Categories", "Categories", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+      
     }
 }
