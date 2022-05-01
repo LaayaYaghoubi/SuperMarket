6c40acd36e1770cd8bb25fa86c96232f00f95ff3
Laaya <layayaghoubi@gmail.com>
2022-05-01 10:40:06 +0430

    super market persistence layer added to the project

---
+++ src/SuperMarket.Persistence.EF/Categories/CategoriesEntityMap.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Persistence.EF.Categories
+{
+    public class BooksEntityMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+           
+        }
+    }
+}
