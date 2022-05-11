9d9674252cabe39d46041f1871c45fdc993cde76
Laaya <layayaghoubi@gmail.com>
2022-05-11 16:18:55 +0430

    product controller added to project

---
+++ src/SuperMarket.RestAPI/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.Produccts.Contracts;
+using SuperMarket.Services.Products.Contracts;
+using System.Collections.Generic;
+
+namespace SuperMarket.RestAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _service;
+
+        public ProductController(ProductService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public void Add(AddProductDto product)
+        {
+            _service.Add(product);
+        }
+
+        [HttpGet]
+        public IList<GetAllProductsDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPut]
+        public void Update(int id, UpdateProductDto product)
+        {
+            _service.Update(id, product);
+        }
+
+    }
+}
