48c248271435fc85d7b78056b6b34b5a084a48ff
Laaya <layayaghoubi@gmail.com>
2022-05-11 16:21:48 +0430

    purchase voucher controller added to project

---
+++ src/SuperMarket.RestAPI/Controllers/PurchaseVoucherController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.PurchaseVouchers.Contracts;
+using System.Collections.Generic;
+
+namespace SuperMarket.RestAPI.Controllers
+{
+    [Route("api/purchase-voucher")]
+    [ApiController]
+    public class PurchaseVoucherController : ControllerBase
+    {
+        private readonly PurchaseVoucherService _service;
+
+        public PurchaseVoucherController(PurchaseVoucherService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public void Add(AddPurchaseVoucherDto PurchaseVoucher)
+        {
+            _service.Add(PurchaseVoucher);
+        }
+
+        [HttpGet]
+        public IList<GetAllPurchaseVoucherDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPut]
+        public void Update(int id, UpdatePurchaseVoucherDto PurchaseVoucher)
+        {
+            _service.Update(id, PurchaseVoucher);
+        }
+
+    }
+}
