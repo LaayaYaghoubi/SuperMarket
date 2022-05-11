352f1c5091faa17e0c0362805696b8aeb0a536ac
Laaya <layayaghoubi@gmail.com>
2022-05-11 16:24:32 +0430

    sales invoice controller added to project

---
+++ src/SuperMarket.RestAPI/Controllers/SalesInvoiceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.SalesInvoices.Contracts;
+using System.Collections.Generic;
+
+namespace SuperMarket.RestAPI.Controllers
+{
+    [Route("api/sales-invoices")]
+    [ApiController]
+    public class SalesInvoiceController : ControllerBase
+    {
+        private readonly SalesInvoiceService _service;
+
+        public SalesInvoiceController(SalesInvoiceService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public void Add(AddSalesInvoiceDto SalesInvoice)
+        {
+            _service.Add(SalesInvoice);
+        }
+
+        [HttpGet]
+        public IList<GetAllSalesInvoiceDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPut]
+        public void Update(int id, UpdateSalesInvoiceDto SalesInvoice)
+        {
+            _service.Update(id, SalesInvoice);
+        }
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+    }
+}
